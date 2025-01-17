20e99009e492fcdd886d85c167ffc63021545996
LEVY Myriam <myriam.levy@cnsa.fr>
2025-01-17 10:43:10 +0100

    corrections pour que les colonnes du tableau renvoyé portent le nom du groupe et de la variable passés en argument

--- R/creer_tableau_synthese.R
+++ R/creer_tableau_synthese.R
@@ -5,27 +5,26 @@ NULL
 #'
 #' Crée un tableau synthétisant la somme d'une variable pour une année sur tous les FINESS ouverts selon un regroupement
 #'
-#' @param finess_full résultat de creer_reference_finess qui contient un tableau 0-1 indiquant par année si un FINESS est ouvert
+#' @param finess_full résultat de creer_reference_finess ou charger_reference_finess qui contient un tableau 0-1 indiquant par année si un FINESS est ouvert
 #' @param tab tableau avec au moins une colonne FINESS et une variable numérique à synthétiser
 #' @param groupe catégorie selon laquelle on veut regrouper : "categetab", "code_regroup_finess", "region" ou "departement"
-#' @param variable variable que l'on veut synthétiser sous forme de chaîne de caractères
-#' @param annee année étudiée sous forme de chaîne de caractères
+#' @param variable variable que l'on veut synthétiser, sous forme de chaîne de caractères
+#' @param annee année étudiée, sous forme de chaîne de caractères
 #'
 #' @return un tableau avec trois colonnes :
-#'  groupe : la liste des catégories ;
-#'  variable : somme des valeurs pour les FINESS ouverts pour chaque catégorie ;
+#'  nom du groupe : la liste des catégories ;
+#'  nom de la variable : somme des valeurs pour les FINESS ouverts pour chaque catégorie ;
 #'  nombre : nombre de FINESS ouverts dans chaque catégorie
 #'
 #' @export
 
 creer_tableau_synthese <- function(finess_full, tab, groupe, variable, annee) {
   resultat <- tab %>%
-    select(FINESS, variable) %>%
-    left_join(finess_full[["CAP_BOOL"]] %>% select(FINESS, groupe, annee), by = "FINESS") %>%
-    rename("groupe" = groupe, "nombre" = annee, "variable" = variable) %>%
-    mutate(variable = variable * nombre) %>%
-    group_by(groupe) %>%
-    summarise(variable = sum(variable, na.rm = T), nombre = sum(nombre, na.rm = T)) %>%
-    select(groupe, variable, nombre)
+    select(FINESS, !!sym(variable)) %>%
+    left_join(finess_full[["CAP_BOOL"]] %>% select(FINESS, !!sym(groupe), !!sym(annee)), by = "FINESS")
+  resultat[[variable]] <- resultat[[variable]] * resultat[[annee]]
+  resultat <- resultat %>%
+    group_by(!!sym(groupe)) %>% summarise(somme = sum(!!sym(variable), na.rm = T), nombre = sum(!!sym(annee), na.rm = T))
+  colnames(resultat)[colnames(resultat) == "somme"] <- variable
   return (resultat)
 }
