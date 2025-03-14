225884babd2507d210577860929ffd8025a7822a
LEVY Myriam <myriam.levy@cnsa.fr>
2025-03-14 09:59:39 +0100

    Ajout du chemin vers la base finess pour le sidata

--- R/charger_reference_finess.R
+++ R/charger_reference_finess.R
@@ -16,6 +16,8 @@ charger_reference_finess <- function(origine = "GEOD") {
     repertoire_finess <- cnsa::config$chemins$finess
   } else if (origine == "VM") {
     repertoire_finess <- cnsa::config$chemins$finess_vm
+  } else if (origine == "SIDATA") {
+    repertoire_finess <- cnsa::config$chemins$finess_sidata
   }
   resultat <- readRDS(paste0(repertoire_finess, "finess_full.rds"))
   return(resultat)
